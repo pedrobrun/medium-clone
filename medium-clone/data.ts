932c4f1a12fa7234b8a02435f768a7186f8f32b7
Pedro Brun <pedroluccavb@gmail.com>
2022-03-14 14:17:17 -0300

    feat: add mock data and its endpoint

---
+++ medium-clone/data.ts
@@ -0,0 +1,32 @@
+const posts = [
+  {
+    id: 1,
+    name: "Elon Musk's life story",
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg',
+    author: 'John Doe',
+  },
+  {
+    id: 2,
+    name: 'How Ukraine-Russia war impacts gasoline price',
+    image:
+      'https://ichef.bbci.co.uk/news/640/cpsprodpb/A025/production/_123579904_global_oil_prices_0822-nc.png',
+    author: 'John Doe Junior',
+  },
+  {
+    id: 3,
+    name: 'Meet Ternoa: The Blockchain of augmented NFTs',
+    image:
+      'https://yt3.ggpht.com/6TM1PZk_cRYOzSQ9eLtJwW62-z3wV8PbShH7kNIHX7zbZ5PumOwsnmld0gnH0Jj5cqubov75tQ=s900-c-k-c0x00ffffff-no-rj',
+    author: 'John Doe',
+  },
+  {
+    id: 4,
+    name: 'How do sunflowers work',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/280px-Sunflower_sky_backdrop.jpg',
+    author: 'Alexia',
+  },
+];
+
+export default posts;
