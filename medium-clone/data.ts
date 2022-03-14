75d0122b3d492f7537229dfab22a41333f43e1c4
Pedro Brun <pedroluccavb@gmail.com>
2022-03-14 17:09:46 -0300

    feat: add content to mock data

--- medium-clone/data.ts
+++ medium-clone/data.ts
@@ -5,6 +5,8 @@ const posts = [
     image:
       'https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg',
     author: 'John Doe',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ const posts = [
     image:
       'https://ichef.bbci.co.uk/news/640/cpsprodpb/A025/production/_123579904_global_oil_prices_0822-nc.png',
     author: 'John Doe Junior',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
     id: 3,
@@ -19,6 +23,8 @@ const posts = [
     image:
       'https://yt3.ggpht.com/6TM1PZk_cRYOzSQ9eLtJwW62-z3wV8PbShH7kNIHX7zbZ5PumOwsnmld0gnH0Jj5cqubov75tQ=s900-c-k-c0x00ffffff-no-rj',
     author: 'John Doe',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
     id: 4,
@@ -26,6 +32,8 @@ const posts = [
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/280px-Sunflower_sky_backdrop.jpg',
     author: 'Alexia',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
 ];
 
